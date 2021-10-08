5af5df6e19df838179a0e60694be2f41bcdda0e3
bluxa <81200014+bluxa@users.noreply.github.com>
2021-10-07 19:23:58 -0600

    prueba de push

--- ProyectoDekkerPeterson/Form1.cs
+++ ProyectoDekkerPeterson/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,38 @@ namespace ProyectoDekkerPeterson
 {
     public partial class Form1 : Form
     {
+        delegate void delegado(int valor);
+
+        public static String GetTimestamp(DateTime value)
+        {
+            return value.ToString("yyyy/MM/dd HH:mm:ss.ffff");
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread H1 = new Thread(new ThreadStart(Atender));
+            Thread H2 = new Thread(new ThreadStart(Atender));
+            H1.Start();
+            H2.Start();
+        }
+
+        public void Atender()
+        {
+            for (int n = 1; n <= Convert.ToInt32(textBox1.Text); n++)
+            {
+                delegado MD = new delegado(ActualizarFactorial);
+                this.Invoke(MD, new object[] { n });
+                Thread.Sleep(20);
+            }
+        }
+
+        public void ActualizarFactorial(int valor)
+        {
+            textBox2.Text = textBox2.Text + GetTimestamp(DateTime.Now) + " - " + "Atendiendo Uso De Hilos Factorial 1:   " + valor + "\r\n";
         }
     }
 }
